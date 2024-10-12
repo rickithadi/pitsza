26de50a0b4e0b0f9ef5ceff3edc0439a95afcd51
Hadi Rickit <rickithadi@gmail.com>
2024-10-12 21:38:28 +0900

    fix carou

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 const Carousel = () => {
   const images = [
-    '/images/logo.jpg',
-    '/images/logo.jpg',
-    '/images/logo.jpg',
+    '/images/car1.jpg',
+    // '/images/car2.jpg',
+    // '/images/car3.jpg',
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,44 +18,55 @@ const Carousel = () => {
   };
 
   return (
-    <div className="relative w-full max-w-4xl mx-auto overflow-hidden max-h-full">
+    <div className="relative w-full mx-auto overflow-hidden  md:h-[600px]">
       {/* Carousel Images */}
       <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
         {images.map((image, index) => (
-          <img
+          <div
             key={index}
-            src={image}
-            alt={`Slide ${index + 1}`}
-            className="w-full flex-shrink-0 object-cover"
-          />
+            className="relative w-full flex-shrink-0 h-full"
+          >
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white p-4">
+              <h2 className="text-4xl font-bold ">bda</h2>
+            </div>
+            <img
+              key={index}
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="w-full object-cover md:object-contain  h-full "
+            />
+
+          </div>
         ))}
       </div>
 
-      {/* Navigation Controls */}
-      <button
-        onClick={handlePrev}
-        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-75"
-      >
-        &#9664;
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-75"
-      >
-        &#9654;
-      </button>
+      {images.length > 1 && (<>
+        {/* Navigation Controls */}
+        <button
+          onClick={handlePrev}
+          className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-75"
+        >
+          &#9664;
+        </button>
+        <button
+          onClick={handleNext}
+          className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-75"
+        >
+          &#9654;
+        </button>
 
-      {/* Dots Navigation */}
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrentIndex(index)}
-            className={`w-3 h-3 rounded-full ${currentIndex === index ? 'bg-white' : 'bg-gray-500'} hover:bg-white`}
-          />
-        ))}
-      </div>
-    </div>
+        {/* Dots Navigation */}
+        <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`w-3 h-3 rounded-full ${currentIndex === index ? 'bg-white' : 'bg-gray-500'} hover:bg-white`}
+            />
+          ))}
+        </div></>)
+      }
+    </div >
   );
 };
 
